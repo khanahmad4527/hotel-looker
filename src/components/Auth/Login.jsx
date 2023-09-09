efc9238b3a73b89f7c009fe56fc46611d9ad43b3
khanahmad4527 <khanahmad4527@gmail.com>
2023-09-09 20:01:40 +0530

    added auth

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { GoogleButton } from "react-google-button";
+import { UserAuth } from "../../hoc/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Flex, Heading } from "@chakra-ui/react";
+
+const Login = () => {
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    }
+  }, [navigate, user]);
+
+  return (
+    <Flex flexDir={"column"} gap={"20px"} mt={"50px"}>
+      <Heading textAlign={"center"}>Sign in</Heading>
+      <Flex justifyContent={"center"}>
+        <GoogleButton onClick={handleGoogleSignIn} />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Login;
